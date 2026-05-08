bad974da57993a85fd11f3acd1459a6cbd66ee14
Sekionn <Sjknudesn@gmail.com>
2026-05-08 10:11:15 +0200

    Fix column names in wrong order

--- env/platformer_env.py
+++ env/platformer_env.py
@@ -339,7 +339,7 @@ class PlatformerEnv(gym.Env):
 
         if not RESULTS_FILE.exists():
             data = [
-            ['timestamp', 'run_label','Player name', 'level', 'outcome', 'ticks', 'inputs', 'resets', 'elapsed_seconds', 'final_reward'],
+            ['timestamp', 'run_label', 'level', 'Player name', 'outcome', 'ticks', 'inputs', 'resets', 'elapsed_seconds', 'final_reward'],
         ]
 
         if self.player_name == "":
